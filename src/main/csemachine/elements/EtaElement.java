1f632e6704c0c7cc16b25f01fab6e2dbe19b9da2
Nimantha Cooray <nimanthacooray@gmail.com>
2022-12-03 21:30:02 +0530

    Add `EtaElement` class

---
+++ src/main/csemachine/elements/EtaElement.java
@@ -0,0 +1,38 @@
+package csemachine.elements;
+
+import csemachine.Environment;
+
+import java.util.ArrayList;
+
+public class EtaElement extends Element {
+    private final int index;
+    private final ArrayList<String> bindingVars;
+    private final Environment environment;
+
+    public EtaElement(int index, ArrayList<String> bindingVars, Environment environment) {
+        this.index = index;
+        this.bindingVars = bindingVars;
+        this.environment = environment;
+    }
+
+    @Override
+    public String getStringRepresentation() {
+        StringBuilder repr = new StringBuilder("eta" + index);
+        for (String var : bindingVars) {
+            repr.append("/").append(var);
+        }
+        return repr.toString();
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public ArrayList<String> getBindingVars() {
+        return bindingVars;
+    }
+
+    public Environment getEnvironment() {
+        return environment;
+    }
+}
